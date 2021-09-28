e2d35add3beccb28c386a58be244e027a774506f
Ibrahim Ramirez <ibrahimramirez085@gmail.com>
2021-09-28 15:21:03 -0500

    version 0.2.0

--- js/Game.js
+++ js/Game.js
@@ -2,7 +2,7 @@ const tablero = document.getElementById('tablero');
 const juego = document.getElementById('game-container');
 const playBtn = document.getElementById('play');
 const volumeBtn = document.getElementById('volume-btn');
-const player = document.getElementById('player');
+
 
 volumeBtn.addEventListener('click', ()=> {
     if(volumeBtn.className == 'fas fa-volume-up'){
@@ -21,3 +21,6 @@ playBtn.addEventListener('click', ()=> {
     juego.classList.add('game-container-on');
     player.classList.add('player');
 });
+
+
+
--- js/Player.js
+++ js/Player.js
@@ -1,9 +1,12 @@
-class Player {
-    constructor(name, score, poweUps){
-        this.name = name;
-        this.score = score;
-        this.poweUps = poweUps;
-    }
+let modifier = 15;
+const player = document.getElementById('player');
+let moveDown = document.addEventListener('keydown', (event)=>{
+    const {style} = player;
+        switch(event.key){
+            case 'ArrowUp': style.bottom = `${parseInt(style.bottom) + modifier}px`; break;
+            case 'ArrowDown': style.bottom = `${parseInt(style.bottom)- modifier}px`; break;
+            case 'ArrowLeft': style.left = `${parseInt(style.left)- modifier}px`; break;
+            case 'ArrowRight': style.left = `${parseInt(style.left)+ modifier}px`; break;
+        }
+});
 
-    
-}
